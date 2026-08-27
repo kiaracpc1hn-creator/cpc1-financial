88a7acd3b873b70cba187e7e433b7458ba11d223
kiaracpc1hn-creator <kiara.cpc1hn@gmail.com>
2026-08-27 10:09:14 +0700

    Add files via upload

---
+++ cpc1-autofill.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         CPC1 AutoFill - Đề nghị thanh toán
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Tự động điền ĐNTT và Hoá đơn từ hệ thống CPC1 vào sanxuat.icpc1hn.work
+// @author       CPC1
+// @match        https://sanxuat.icpc1hn.work/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.log("[CPC1 AutoFill Userscript] Sẵn sàng hoạt động trên sanxuat.icpc1hn.work!");
+})();
